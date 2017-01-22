a12b62ee2828e4af475731e5ce0c8f5ce5af61f9
Krzysztof Hamerszmit <kkhamer@gmail.com>
2017-01-22 23:41:04 +0100

    Add button and getWarannyCost function

--- js/script.js
+++ js/script.js
@@ -7,17 +7,30 @@ function Telefon(marka, cena, kolor) {
 
 //metody
 Telefon.prototype.printInfo = function() {
-	console.log("Marka telefonu to " + this.marka + ", kolor " + this.kolor + ", a cena to " + this.cena + ".");
-	return("Marka telefonu to " + this.marka + ", kolor " + this.kolor + ", a cena to " + this.cena + ".");
+	return "Marka telefonu to " + this.marka + ", kolor " + this.kolor + ", a cena to " + this.cena + ".\n" + "Koszt przedłużonej gwarancji: " + this.getWarranyCost() +"\n\n";
 }
 
 Telefon.prototype.sumPrice = function(number) {
-	var number = number;
-	console.log("Cena wszystkich produktów to " + this.cena * number);
+	return "Cena wszystkich produktów to " + this.cena * number;
 }
 
+Telefon.prototype.getWarranyCost = function() {
+	return this.cena * 0.1;
+};
 
 //obiekty
 var SamsungGalaxyS6 = new Telefon('Samsung', 1600, 'czarny');
 var iPhone6s = new Telefon('Apple', 2600, 'biały');
-var iOnePlus = new Telefon('One', 600, 'srebrny');
+var OnePlus = new Telefon('One', 600, 'srebrny');
+
+console.log(iPhone6s.printInfo() +" " + OnePlus.printInfo() + " " + SamsungGalaxyS6.printInfo());
+
+//BUTTONS
+$(function() {
+	var orderIphone = $('button');
+
+	orderIphone.click(function() {
+		var howMany = prompt('Ile sztuk?');
+		console.log(iPhone6s.sumPrice(howMany));
+	})
+});
